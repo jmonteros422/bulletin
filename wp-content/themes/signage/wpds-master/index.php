310325fe9ca41a937fb335134e83e73e3ea68e61
janmonteros <janraymonteros@gmail.com>
2016-10-19 21:53:46 +0800

    Video Adjustments

--- wp-content/themes/signage/wpds-master/index.php
+++ wp-content/themes/signage/wpds-master/index.php
@@ -43,11 +43,16 @@ get_header(); ?>
 							</video>
 						</div>
 					<?php else: ?>
-						<div class="video">
-							<video class="videoContent" data-autoplay="true" controls width="100%">
-								<source src="<?php echo get_post_meta($post->ID, 'video-link', true) ?>" type="video/mp4" />
-							</video>
+						<div class="videoWrapper">
+								<iframe class="videoContent" src="<?php echo get_post_meta($post->ID, 'video-link', true) ?>" frameborder="0" allowfullscreen data-autoplay="true"></iframe>
 						</div>
+<!--						<div style="width:100%;height:100%;width: 820px; height: 461.25px; float: none; clear: both; margin: 2px auto;">-->
+<!--							<embed src="--><?php //echo get_post_meta($post->ID, 'video-link', true) ?><!--?version=3&amp;hl=en_US&amp;rel=0&amp;autohide=1&amp;autoplay=1" wmode="transparent" type="application/x-shockwave-flash" width="100%" height="100%" allowfullscreen="true" title="Adobe Flash Player">-->
+<!--						</div>-->
+
+<!--						<object class="videoContent" data="http://www.youtube.com/embed/GlIzuTQGgzs">-->
+<!--						</object>-->
+
 
 					<?php endif; ?>
 
@@ -61,7 +66,7 @@ get_header(); ?>
 	            endif;
 				wp_reset_query();
 				?>
-	            ?>
+
 			</ul>
 	</div>
 </div>
